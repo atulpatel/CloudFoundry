b61450112df1e797bbe80e7a042f1837422fdea0
Atul Kumar Patel <atulpatel.mca@gmail.com>
2018-11-12 12:14:32 -0800

    Initial Draft

---
+++ AspnetCoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspnetCoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
